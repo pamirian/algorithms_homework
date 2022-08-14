7eff50365486b68926e9004d0c8d4b00d88adde0
pamirian <mirzoshoev@gmail.com>
2022-08-15 00:32:02 +0200

    correct braces

---
+++ 11-08-2022/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack st1 = new Stack();
+        String str = "((({{{[[[]]]}}})))";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if ((c == '(')|(c == '{')|(c == '[')) {
+                st1.push(c);
+            } else {
+                if (st1.isEmpty()) {
+                    System.out.println("Wrong bracket string");
+                    return;
+                }
+                if ((c == ')')|(c == '}')|(c == ']')) {
+                    st1.pop();
+                }
+            }
+        }
+        if (st1.isEmpty()) {
+            System.out.println("Correct bracket string");
+        } else {
+            System.out.println("Wrong bracket string");
+        }
+    }
+}
---
+++ 11-08-2022/src/com/company/Stack.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Stack {
+
+    StackItem top;
+
+    public void push(char item) {
+        StackItem temp = new StackItem();
+        temp.value = item;
+        temp.next = top;
+        top = temp;
+    }
+
+    public char pop() {
+        char a = top.value;
+        top = top.next;
+        return a;
+    }
+
+    // Проверка пуст ли стек
+    public boolean isEmpty() {
+        return (top == null);
+    }
+
+    private class StackItem {
+        char value;
+        StackItem next;
+    }
+}
